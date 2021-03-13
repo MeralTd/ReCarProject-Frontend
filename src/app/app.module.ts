495d0c01437c93f44e5451c091014146f3bd7ee3
MeralTd <meraltasdemir77@gmail.com>
2021-03-13 21:13:36 +0300

    Add Models and Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarComponent } from './components/car/car.component';
 import { BrandComponent } from './components/brand/brand.component';
 import { NaviComponent } from './components/navi/navi.component';
+import { ColorComponent } from './components/color/color.component';
+import { RentalComponent } from './components/rental/rental.component';
+import { CustomersComponent } from './components/customers/customers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CarComponent,
     BrandComponent,
-    NaviComponent
+    NaviComponent,
+    ColorComponent,
+    RentalComponent,
+    CustomersComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/rental.ts
@@ -0,0 +1,6 @@
+export interface Rental{
+    id:number;
+    carName:string;
+    customerName:string;
+    companyName:string;
+}
